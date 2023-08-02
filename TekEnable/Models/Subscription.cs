71fdf17685a068fefa73494e3ae5c924d2304794
murideous <murideous2012@gmail.com>
2023-08-02 14:44:39 +0100

    Add project files.

---
+++ TekEnable/Models/Subscription.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TekEnable.Models
+{
+    public class Subscription
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+       
+        [Required]
+        public string HowYouHeard { get; set; }
+
+        public string SignUpReason { get; set; }
+    }
+}
